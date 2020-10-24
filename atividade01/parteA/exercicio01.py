7c7e21497fb016775c284010c1d9cd8b02ca3d59
boa50 <bruno.albuquerque50@gmail.com>
2020-10-24 06:11:32 -0300

    Implementação inicial da bilienar

--- atividade01/parteA/exercicio01.py
+++ atividade01/parteA/exercicio01.py
@@ -1,6 +1,7 @@
 # Referencias
 # https://www.imageeprocessing.com/2017/11/nearest-neighbor-interpolation.html
 # https://gist.github.com/KeremTurgutlu/68feb119c9dd148285be2e247267a203
+# https://chao-ji.github.io/jekyll/update/2018/07/19/BilinearResize.html
 
 import numpy as np
 import cv2
@@ -12,7 +13,7 @@ def show_img(img, title='image'):
     cv2.waitKey(0)  
     cv2.destroyAllWindows()
 
-def interpolate_loop(size_new, row_positions, col_positions, blue, green, red):
+def interpolate_neighbors_loop(size_new, row_positions, col_positions, blue, green, red):
     b = np.zeros((size_new[0], size_new[1]))
     g = np.zeros((size_new[0], size_new[1]))
     r = np.zeros((size_new[0], size_new[1]))
@@ -24,22 +25,25 @@ def interpolate_loop(size_new, row_positions, col_positions, blue, green, red):
 
     return b, g, r
 
-def interpolate_vectorized(row_positions, col_positions, blue, green, red):
+def interpolate_neighbors_vectorized(row_positions, col_positions, blue, green, red):
     b = blue[row_positions, :][:, col_positions]
     g = green[row_positions, :][:, col_positions]
     r = red[row_positions, :][:, col_positions]
 
     return b, g, r
 
-def interpolate(img, size_new, row_positions, col_positions, method='vectorized'):
+def interpolate_neighbors(img, size_new, fator, method='vectorized'):
+    row_positions = (np.ceil((np.arange(size_new[0]) + 1) / fator) - 1).astype(int)
+    col_positions = (np.ceil((np.arange(size_new[1]) + 1) / fator) - 1).astype(int)
+
     blue = img[:, :, 0]
     green = img[:, :, 1]
     red = img[:, :, 2]
 
     if method == 'vectorized':
-        b, g, r = interpolate_vectorized(row_positions, col_positions, blue, green, red)
+        b, g, r = interpolate_neighbors_vectorized(row_positions, col_positions, blue, green, red)
     elif method == 'loop':
-        b, g, r = interpolate_loop(size_new, row_positions, col_positions, blue, green, red)
+        b, g, r = interpolate_neighbors_loop(size_new, row_positions, col_positions, blue, green, red)
     else:
         print('Método não disponível')
         return
@@ -51,6 +55,47 @@ def interpolate(img, size_new, row_positions, col_positions, method='vectorized'
 
     return img_resized
 
+def interpolate_bilinear(img, size_new, fator):
+    blue = img[:, :, 0]
+    green = img[:, :, 1]
+    red = img[:, :, 2]
+
+    b = np.empty(size_new, dtype=np.uint8)
+
+    for row in range(size_new[0]):
+        for col in range(size_new[1]):
+            x_new = (1/fator) * row
+            y_new = (1/fator) * col
+
+            x1 = np.floor(x_new).astype(int)
+            y1 = np.floor(y_new).astype(int)
+            x2 = np.ceil(x_new).astype(int)
+            y2 = np.ceil(y_new).astype(int)
+
+            #limite final das posições da imagem
+            if x2 == img.shape[:2][0]:
+                x2 -= 1
+
+            if y2 == img.shape[:2][1]:
+                y2 -= 1
+
+            dx = x_new - x1
+            dy = y_new - y1
+
+            A = blue[x1][y1]
+            B = blue[x2][y1]
+            C = blue[x1][y2]
+            D = blue[x2][y2]
+
+            b1 = (1 - dx) * (1 - dy) * A
+            b2 = dx * (1 - dy) * B
+            b3 = (1 - dx) * dy * C
+            b4 = dx * dy * D
+
+            b[row][col] = b1 + b2 + b3 + b4
+
+    return b
+
 def mudanca_resolucao(img, fator, method='vizinho'):
     size_original = np.array(img.shape[:2])
     size_new = (size_original * fator).astype(int)
@@ -58,13 +103,9 @@ def mudanca_resolucao(img, fator, method='vizinho'):
     img_resized = None
 
     if method == 'vizinho':
-        row_positions = (np.ceil((np.arange(size_new[0]) + 1) / fator) - 1).astype(int)
-        col_positions = (np.ceil((np.arange(size_new[1]) + 1) / fator) - 1).astype(int)
-
-        img_resized = interpolate(img, size_new, row_positions, col_positions)
+        img_resized = interpolate_neighbors(img, size_new, fator)
     elif method == 'bilinear':
-        print('Método não implementado')
-        pass
+        img_resized = interpolate_bilinear(img, size_new, fator)
     else:
         print('Método não disponível')
         pass
@@ -93,11 +134,15 @@ if __name__ == "__main__":
     #     show_img(img_aumentada, title=f'Imagem reduzida pelo fator {fator}')
 
     #c
-    img_reduzidas = []
+    # img_reduzidas = []
 
-    for fator in fator_reducoes:
-        img_reduzidas.append(mudanca_resolucao_path(img_folder + 'lenna.png', fator, 'bilinear'))
+    # for fator in fator_reducoes:
+    #     img_reduzidas.append(mudanca_resolucao_path(img_folder + 'lenna.png', fator, 'bilinear'))
 
-    for fator, img in zip(fator_aumentos, img_reduzidas):
-        img_aumentada = mudanca_resolucao(img, fator, 'bilinear')
-        show_img(img_aumentada, title=f'Imagem reduzida pelo fator {fator}')
+    # for fator, img in zip(fator_aumentos, img_reduzidas):
+    #     img_aumentada = mudanca_resolucao(img, fator, 'bilinear')
+    #     show_img(img_aumentada, title=f'Imagem reduzida pelo fator {fator}')
+
+    fator = 0.5
+    test = mudanca_resolucao_path(img_folder + 'lenna.png', fator, 'bilinear')
+    show_img(test, title=f'Imagem reduzida pelo fator {fator}')

d2842331df8d8c591b609c95fbc88dcf8d2afeaa
boa50 <bruno.albuquerque50@gmail.com>
2020-11-08 08:37:37 -0300

    Finalização do exercício 03

--- atividade01/parteA/exercicio03.py
+++ atividade01/parteA/exercicio03.py
@@ -3,6 +3,8 @@ import cv2
 from cv2 import imread
 import matplotlib.pyplot as plt
 
+from exercicio02 import filtra_espacial
+
 def show_img(imgs, titles, rgb=True):
     width = 4 * len(imgs)
     height = 4
@@ -26,7 +28,7 @@ def converte_dominio_frequencia(filtro, tamanho=None):
     if tamanho == None:
         return np.fft.fft2(filtro)
     else:
-        return np.fft.fftn(filtro, s=tamanho)
+        return np.fft.fft2(filtro, s=tamanho)
 
 def shift_freq_center(filtro):
     center = filtro.shape[0]//2
@@ -65,6 +67,24 @@ def get_modulo(filtro):
 
     return modulo
 
+def convolve_freq(img, filtro):
+    ### (aplicação dos filtros = multiplica os dois no domínio da frequência depois calcula a transformada inversa)
+    ### (a multiplicação deve ser pixel a pixel, usar o parâmetro s (shape))
+    ### (aula 03 1:04)
+
+    shape = img.shape[:2]
+    filtro_freq = converte_dominio_frequencia(filtro, tamanho=shape)
+            
+    img_filtered = np.empty(img.shape, dtype='uint8')
+    for c in range(img.shape[2]):
+        c_freq = converte_dominio_frequencia(img[:, :, c])
+        conv_freq = c_freq * filtro_freq
+        channel = np.fft.ifft2(conv_freq)
+        channel = channel.clip(min=0, max=255).astype('uint8')
+        img_filtered[:, :, c] = channel
+
+    return img_filtered
+
 if __name__ == "__main__":
     img_folder = 'atividade01/parteA/imagens/'
 
@@ -80,45 +100,29 @@ if __name__ == "__main__":
     ### a
     # imgs = []
     # for filtro in filtros:
+    #     filtro_freq = converte_dominio_frequencia(filtro)
+    #     filtro_mod = get_modulo(filtro_freq)
+    #     imgs.append(filtro_mod)
+
     #     filtro_freq = converte_dominio_frequencia(filtro, tamanho=(256, 256))
     #     filtro_mod = get_modulo(filtro_freq)
     #     imgs.append(filtro_mod)
 
-    # titles = ['Módulo de h1', 'Módulo de h2']
+    # titles = ['Módulo de h1', 'Módulo de h1 (expandido para 256 x 256)', 'Módulo de h2', 'Módulo de h2 (expandido para 256 x 256)']
 
     # show_img(imgs, titles, rgb=False)
 
     ### b 
-    ### (aplicação dos filtros = multiplica os dois no domínio da frequência depois calcula a transformada inversa)
-    ### (a multiplicação deve ser pixel a pixel, usar o parâmetro s (shape))
-    ### (aula 03 1:04)
-    img_paths = [img_folder + 'minhas/floresta.jpg',
-                img_folder + 'minhas/montanha.jpg',
-                img_folder + 'minhas/praia.jpg'] 
+    # img_paths = [img_folder + 'minhas/floresta.jpg',
+    #             img_folder + 'minhas/montanha.jpg',
+    #             img_folder + 'minhas/praia.jpg'] 
 
-    for path in img_paths:
-        imgs = []
-        img = imread(path)
-        # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        shape = img.shape[:2]
+    # for path in img_paths:
+    #     img = imread(path)
+    #     imgs = [img]
         
-        img_freq = converte_dominio_frequencia(img)
-        
-        for filtro in filtros:
-            filtro_freq = converte_dominio_frequencia(filtro, tamanho=shape)
-            # filtro_freq = filtro_freq[:, :, None] * np.ones(1, dtype=int)[None, None, :]
-            
-            img_new = np.empty((256, 256, 3), dtype=np.complex128)
-            for c in range(3):
-                conv_freq = img_freq[:, :, c] * filtro_freq
-                img_new[:, :, c] = np.fft.ifft2(conv_freq)
+    #     for filtro in filtros:
+    #         imgs.append(convolve_freq(img, filtro))
+    #         imgs.append(filtra_espacial(path, filtro))
 
-            # conv_freq = img_freq * filtro_freq
-            # img_new = np.fft.ifft2(conv_freq)
-
-            # img = img_normalize(img_new)
-            img = get_modulo(img_new)
-            
-            imgs.append(img)
-
-        show_img(imgs, ['', ''], rgb=False)
+    #     show_img(imgs, ['Original', 'Filtro h1 (freqência)', 'Filtro h1 (espaço)', 'Filtro h2 (frequência)', 'Filtro h2 (espaço)'])
